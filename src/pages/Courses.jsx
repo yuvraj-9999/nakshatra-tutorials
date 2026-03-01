ef4a65c02bb7f4c304315ce32122311555189c50
Yuvraj Sabale <yuvrajsabale999@gmail.com>
2026-03-01 18:36:23 +0530

    Fix case-sensitive import

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { coursesData } from "../data/CoursesData";
+import { coursesData } from "../data/coursesData";
 import { motion, AnimatePresence } from "framer-motion"
 import Reveal from "../components/Reveal";
 
